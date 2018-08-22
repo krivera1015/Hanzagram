5e008cbe32778374789bbc73a0a749f322af5730
personabe1984 <personabe1984@gmail.com>
2018-08-22 14:56:58 -0400

    When the user is first created they should create the first post

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,13 @@ class SessionsController < ApplicationController
   end
 
   def create
+    # Find the user
     @user = User.find_by(username: params[:username])
+
     if @user && @user.authenticate(params[:password])
-        session[:user_id] = @user.id
+      session[:user_id] = @user.id
+      # We want to show all of the post that are tied to this user
+      redirect_to posts_path
     else
       redirect_to login_path
     end
