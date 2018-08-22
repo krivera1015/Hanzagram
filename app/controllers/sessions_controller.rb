c8a1659516136d97792092faca5d4b5880c6eb68
Kevin RRivera <krivera1015@gmail.com>
2018-08-22 09:43:50 -0400

    Routes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,15 @@
 class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    @user = User.find_by(name: params[:name])
+    if @user && @user.authenticate(params[:password])
+        session[:user_id] = @user.id
+    else
+      redirect_to login_path
+    end
+  end
+  
 end
