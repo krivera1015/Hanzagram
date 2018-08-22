fb92c556748f13db3cad78c5a86aff1bd298e5bf
personabe1984 <personabe1984@gmail.com>
2018-08-22 14:43:51 -0400

    Changed the name to username in SeesionsController to match the new.thml.erb form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,12 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(name: params[:name])
+    @user = User.find_by(username: params[:username])
     if @user && @user.authenticate(params[:password])
         session[:user_id] = @user.id
     else
       redirect_to login_path
     end
   end
-  
+
 end
