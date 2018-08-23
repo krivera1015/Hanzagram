f71f7121b75ddb82c381bfe470cb33026a90f231
Kevin RRivera <krivera1015@gmail.com>
2018-08-23 18:45:15 -0400

    Profile Page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def profile_avatar(user)
+    @user = User.find(session[:user_id])
+    if user.avatar.attachment.present?
+      image_tag user.avatar
+    else
+      image_tag 'default-avatar.jpg'
+    end
+  end
 end
