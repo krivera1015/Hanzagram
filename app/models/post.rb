9e0ec3daefd9086b5c5a3065fe467e583c17bb03
personabe1984 <personabe1984@gmail.com>
2018-08-22 21:52:33 -0400

    Drew a relationship with the comment to the user and the post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,9 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :comments, dependent: :destroy
+  
   has_one_attached :image
+
   validates :caption, presence: true
   validates :user_id, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   validates :username, presence: true, length: { minimum: 4, maximum: 16}
   has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
+
   # has_one_attached :avatar
   has_secure_password
 
