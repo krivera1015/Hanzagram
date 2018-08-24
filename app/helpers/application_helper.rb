783f9ba72636760528d4a5568f40a33e46b8fb15
Kevin RRivera <krivera1015@gmail.com>
2018-08-24 06:14:04 -0400

    Avatar resized

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   def profile_avatar(user)
     @user = User.find(session[:user_id])
     if user.avatar.attachment.present?
-      image_tag user.avatar
+      image_tag user.avatar.variant(resize: "150x150")
     else
       image_tag 'default-avatar.jpg'
     end
